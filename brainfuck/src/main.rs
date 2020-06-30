621a03442b6f0e7db021173957e110675eb09623
cafehaine <kilian.guillaume@gmail.com>
2020-06-30 19:43:34 +0200

    Brainfuck: implement program execution

--- brainfuck/src/main.rs
+++ brainfuck/src/main.rs
@@ -22,7 +22,7 @@ struct BrainfuckToken {
 
 fn lexer(program: &String, instructions: &mut Vec<BrainfuckToken>) {
     let mut line: usize = 1;
-    let mut character: usize = 0;
+    let mut character: usize = 1;
     for chr in program.chars() {
         match chr {
             '+' => {
@@ -91,14 +91,14 @@ fn lexer(program: &String, instructions: &mut Vec<BrainfuckToken>) {
             },
             '\n'=> {
                 line += 1;
-                character = 0
+                character = 1
             },
             _    => character += 1,
         }
     }
 }
 
-fn main() -> std::io::Result<()>{
+fn execute(tokens: &Vec<BrainfuckToken>) {
     // Define a new memory buffer for the brainfuck program.
     // This buffer uses 8bit long unsigned integers that overflow.
     // The buffer is 256 elements long.
@@ -106,14 +106,86 @@ fn main() -> std::io::Result<()>{
     // The memoryp pointer used by the brainfuck program also wraps arround.
     let mut mem_pointer = Wrapping(0u8);
 
-    let program = read_to_string("program.bf")?;
-
-    let mut tokens = Vec::new();
-    lexer(&program, &mut tokens);
     let mut token_pointer = 0;
 
     while token_pointer < tokens.len() {
-        token_pointer += 1;
+        let token = &tokens[token_pointer];
+        match token.bftype {
+            BrainfuckTokenType::ValueIncrement => {
+                memory[mem_pointer.0 as usize] += Wrapping(1);
+                token_pointer += 1;
+            },
+            BrainfuckTokenType::ValueDecrement => {
+                memory[mem_pointer.0 as usize] -= Wrapping(1);
+                token_pointer += 1;
+            },
+            BrainfuckTokenType::PointerIncrement => {
+                mem_pointer += Wrapping(1);
+                token_pointer += 1;
+            },
+            BrainfuckTokenType::PointerDecrement => {
+                mem_pointer -= Wrapping(1);
+                token_pointer += 1;
+            },
+            BrainfuckTokenType::LoopStart => {
+                if memory[mem_pointer.0 as usize].0 != 0 {
+                    token_pointer += 1;
+                } else {
+                    let mut loop_end_to_find = 1;
+                    let mut new_token_pointer = token_pointer + 1;
+                    while token_pointer < tokens.len() && loop_end_to_find != 0 {
+                        match tokens[new_token_pointer].bftype {
+                            BrainfuckTokenType::LoopStart => loop_end_to_find += 1,
+                            BrainfuckTokenType::LoopEnd => loop_end_to_find -= 1,
+                            _ => (),
+                        }
+                        new_token_pointer += 1;
+                    }
+                    if loop_end_to_find != 0 {
+                        panic!("Unexpected EOF while looking for end of loop starting at line {} char {}", token.line, token.character);
+                    }
+                    token_pointer = new_token_pointer;
+                }
+            },
+            BrainfuckTokenType::LoopEnd => {
+                if memory[mem_pointer.0 as usize].0 == 0 {
+                    token_pointer += 1;
+                } else {
+                    let mut loop_start_to_find = 1;
+                    let mut new_token_pointer = token_pointer;
+                    while token_pointer > 0 && loop_start_to_find != 0 {
+                        new_token_pointer -= 1;
+                        match tokens[new_token_pointer].bftype {
+                            BrainfuckTokenType::LoopStart => loop_start_to_find -= 1,
+                            BrainfuckTokenType::LoopEnd => loop_start_to_find += 1,
+                            _ => (),
+                        }
+                    }
+                    if loop_start_to_find != 0 {
+                        panic!("Unexpected start of file while looking for start of loop ending at line {} char {}", token.line, token.character);
+                    }
+                    token_pointer = new_token_pointer;
+                }
+            }
+            BrainfuckTokenType::PutChar => {
+                print!("{}", memory[mem_pointer.0 as usize].0 as char);
+                token_pointer += 1;
+            },
+            _ => {
+                panic!("Unhandled brainfuck token type at line {} char {}", token.line, token.character);
+            }
+        }
     }
+}
+
+fn main() -> std::io::Result<()>{
+    let program = read_to_string("program.bf")?;
+
+    let mut tokens = Vec::new();
+
+    lexer(&program, &mut tokens);
+
+    execute(&tokens);
+
     Ok(())
 }
